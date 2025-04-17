a50d1e9e730cc5831587e3c1c7cdf6d934216664
wndudrla1011 <wndudrla1011@naver.com>
2025-04-17 14:10:47 +0900

    test p3_4(연속 부분수열)

--- src/main/java/two_pointer/p3_4/Main2.java
+++ src/main/java/two_pointer/p3_4/Main2.java
@@ -1,26 +1,30 @@
-package two_pointer.연속부분수열;
+package two_pointer.p3_4;
 
 import java.io.IOException;
 
-public class Main {
+public class Main2 {
 
     public static void main(String[] args) throws IOException {
         int N = readInt();
         int M = readInt();
+        int ans = 0;
         int[] A = new int[N];
-        int lt = 0, rt = 0, sum = 0, ans = 0;
 
         for (int i = 0; i < N; i++) {
             A[i] = readInt();
         }
 
-        while (true) {
-            if (sum >= M) {
-                if (sum == M) ans++;
-                sum -= A[lt++];
+        for (int i = 0; i < N - 1; i++) {
+            A[i + 1] += A[i];
+        }
+
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (A[i] - A[j] >= M) {
+                    if (A[i] - A[j] == M) ans++;
+                    break;
+                }
             }
-            else if (rt >= N) break;
-            else sum += A[rt++];
         }
 
         System.out.println(ans);
